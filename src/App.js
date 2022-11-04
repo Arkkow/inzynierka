c230b3a58c1a77f01bcc3fc5e457a105eb30e956
Lewy <pawlew2@st.amu.edu.pl>
2022-11-04 13:54:28 +0100

    Components... not working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import React from "react";
 import './App.css';
+import Tournament_Box from './components/calendar_single/tournament_box'
+
+import Card from "react-bootstrap/Card";
+
 
 function App() {
   return (
-    <div className="App">
+    <Card style={{ width: "22rem" }}>
+        <Card.Body>
+            <Card.Title style={{ color: "green" }}>GEEKSFORGEEKS</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">
+                One Stop For all CS subjects
+            </Card.Subtitle>
+            <Card.Text>
+                GeeksforGeeks provides a platform for all the students to study
+                about all the subjects in CSE.
+            </Card.Text>
+            <Card.Link href="#"> For Students</Card.Link>
+        </Card.Body>
+    </Card>
 
-    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import Tournament_box from "./components/calendar_single/tournament_box.js";
+import BasicExample from "./components/Single_Card/Card_tour";
+import Todo from "./components/Single_Card/Card_tour";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+      <Todo></Todo>
   </React.StrictMode>
 );
 
