0b4b1c05eb9190cac8d23bdaeecd9b9dbbc42b90
lewy <pllewyx@gmail.com>
2022-11-09 13:09:49 +0100

    ADD single example card

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 // General React imports
-import * as React from 'react';
-import { render } from 'react-dom';
+import * as React from "react";
+import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./components/redux/redux_store"
+import { store } from "./components/redux/redux_store";
 
 // Project specific files
-import App from './app';
+import App from "./app";
 
 render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>,
-    document.getElementById("root")
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
--- src/routes/calendar_route.js
+++ src/routes/calendar_route.js
@@ -1,17 +1,19 @@
 // General React imports
-import * as React from 'react';
+import * as React from "react";
 
 // Project specific files
 
 // CSS files
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/index.css';
-import '../styles/App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/index.css";
+import "../styles/App.css";
+import SingleCard from "../components/single_card/single_card";
 
-export default function CalendarRoute () {
-    return (
-        <div>
-            <h2>Calendar</h2>
-        </div>
-    )
-};
+export default function CalendarRoute() {
+  return (
+    <div>
+      <h2>Calendar</h2>
+      <SingleCard />
+    </div>
+  );
+}
