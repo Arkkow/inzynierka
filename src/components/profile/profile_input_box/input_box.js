1285260c86c65a1deb084c31c410df9022450c5e
Jan Majchrzycki <majchrzycki2000@gmail.com>
2022-11-15 11:56:51 +0100

    Wyswietlanie profilu i edycja zrobione

--- src/components/profile/profile_input_box/input_box.js
+++ src/components/profile/profile_input_box/input_box.js
@@ -6,10 +6,10 @@ function InputBox() {
         <div className="mb-3" style={{paddingTop: "3%", paddingLeft: "3%", fontFamily: 'Montserrat', fontWeight: "600", fontSize: "18px", lineHeight: "22px", color: "var(--dark_grey)"}}>
 
             <label style={{display: "block", textAlign: "left"}} htmlFor="exampleFormControlInput1" className="form-label">Twoje ID</label>
-            <input style={{width:"33%"}} type="text" readonly class="form-control-plaintext" id="staticID" value="ID z bazy danych"></input>
+            <input style={{width:"33%"}} type="text" readOnly className="form-control-plaintext" id="staticID" value="ID z bazy danych"></input>
 
 
-            <label style={{display: "block", textAlign: "left", marginTop: "1%"}} htmlFor="exampleFormControlInput1" className="form-label">Twoje ID</label>
+            <label style={{display: "block", textAlign: "left", marginTop: "1%"}} htmlFor="exampleFormControlInput1" className="form-label">Login</label>
             <input style={{width: "33%"}} type="text" readOnly className="form-control-plaintext" id="staticLogin" value="Login z bazy danych"></input>
 
             <label style={{display: "block", textAlign: "left", width:"33%", marginTop: "1%"}} htmlFor="exampleFormControlInput1" className="form-label">E-mail</label>
@@ -24,7 +24,16 @@ function InputBox() {
             <label style={{display: "block", textAlign: "left", width:"33%", marginTop: "1%"}} htmlFor="exampleFormControlInput1" className="form-label">Telefon</label>
             <input style={{width:"33%"}} type="tel" className="form-control" id="exampleFormControlInput1" placeholder="123456789"></input>
 
-            {/*TODO POZOSTALE POLA I OGARNAC Z MIN_HEIGHT ZEBY TE BIALE PROSTOKATY Z FORMSOW GIT DZIALALY*/}
+            <label style={{display: "block", textAlign: "left", width:"33%", marginTop: "1%"}} htmlFor="exampleFormControlInput1" className="form-label">Nowe hasło</label>
+            <input style={{width:"33%"}} type="text" className="form-control" id="exampleFormControlInput1"></input>
+
+            <label style={{display: "block", textAlign: "left", width:"33%", marginTop: "1%"}} htmlFor="exampleFormControlInput1" className="form-label">Powtórz nowe hasło</label>
+            <input style={{width:"33%"}} type="text" className="form-control" id="exampleFormControlInput1"></input>
+
+                <div style={{marginTop: "5%"}}>
+                        <button style={{fontFamily: 'Montserrat', fontWeight: "600", fontSize: "18px", lineHeight: "25px", color: "white"}} type="button" className="btn btn-success">WPROWADŹ ZMIANY</button>
+                </div>
+
 
         </div>
 
--- src/routes/calendar_route.js
+++ src/routes/calendar_route.js
@@ -1,6 +1,7 @@
 // General React imports
 import * as React from "react";
 
+
 // Project specific files
 
 // CSS files
@@ -8,12 +9,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/index.css";
 import "../styles/App.css";
 import SingleCard from "../components/single_card/single_card";
+import Popup from "../components/Popups/Popup"
 
 export default function CalendarRoute() {
   return (
           <div>
       <h2>Calendar</h2>
       <SingleCard />
+
+              <Popup />
+
+
     </div>
   );
 }
