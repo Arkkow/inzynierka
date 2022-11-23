9fb1ad927e55b8b6566f20dc7dc617b75684f164
Lewy <pawlew2@st.amu.edu.pl>
2022-11-23 15:46:30 +0100

    UPDATE test-usunięcie

--- src/app.js
+++ src/app.js
@@ -23,9 +23,6 @@ function App() {
   return (
       <div>
           <Header />
-          <div>
-              TESTOWY DIV DO USUNIĘCIA ASAP
-          </div>
           <Routes>
             <Route path="/#" element={<CalendarRoute />} exact={true} />
             <Route path="/calendar" element={<CalendarRoute />} exact={true} />
