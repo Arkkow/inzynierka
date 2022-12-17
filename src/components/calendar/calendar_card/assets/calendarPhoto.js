5fff43de89d16fdfb8d8db67290ca8265cb51feb
Tomasz Sidoruk <tomsid@st.amu.edu.pl>
2022-12-17 11:15:28 +0100

    ok

--- src/components/calendar/calendar_card/assets/calendarPhoto.js
+++ src/components/calendar/calendar_card/assets/calendarPhoto.js
@@ -8,31 +8,6 @@ import {getImage} from "../../../api/tournament/tournament_CRUD_api.js";
 import cup_logo from "../../../../assets/cup.svg";
 
 export const CalendarPhoto = (props) => {
-	   var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState === 4) {
-		if(this.status === 200){
-			if(this.response.size !=0){
-				this.response.arrayBuffer().then((a)=>{
-									    let binary = '';
-let bytes = new Uint8Array(a);
-    let len = bytes.byteLength;
-    for (let i = 0; i < len; i++) {
-        binary += String.fromCharCode(bytes[i]);
-    }
-			document.getElementById("tid_"+props.id).src="data:image/gif;base64,"+btoa(binary) ;
-				});
-
-			}
-			
-    }else{
-	}
-	}
-};
-xhttp.responseType = "blob";
-xhttp.open("GET", "https://dragonmaster.pl/inz/tournament/image?id="+props.id);
-xhttp.send(); 
-
 
     return (
         <>
@@ -43,7 +18,7 @@ xhttp.send();
                 height: "100%",
                 alignItems: "center"
             }}>
-<img id={"tid_"+props.id} style={{height: "80px", width: "80px"}} src={cup_logo} alt={""} />
+<img  style={{height: "80px", width: "80px"}} src={(props.hasImage == 0 ?cup_logo:"https://dragonmaster.pl/inz/tournament/image?id="+props.id)} alt={""} />
             </div>
         </>
     );
