990986f1608ed2fe77ead5a93e2748aa5d442625
Arek <arkkow98@gmail.com>
2023-01-10 00:50:09 +0100

    Dzialajace ukrycie zakonczenia turnieju

--- src/components/tournament/sections/drabinka/drabinka.js
+++ src/components/tournament/sections/drabinka/drabinka.js
@@ -23,118 +23,126 @@ export const Drabinka = (props) => {
             props.handleDownloadLadders(props.calendar_list.id)
             props.handleDownloadUser()
         }
-        ,[])
+        , [])
 
-    const [chosen_match, set_chosen_match] = useState(() => { return 1; } );
+    const [chosen_match, set_chosen_match] = useState(() => {
+        return 1;
+    });
 
-    let ladders = [{id:0}]
+    let ladders = [{id: 0}]
     ladders = getLaddersFiltered(props.calendar_list.places, props.ladders_list.ladders["ALL"])
 
     let min_round = 0;
     let max_round = 0;
 
-    if(ladders[chosen_match][0] !== undefined) {
+    if (ladders[chosen_match][0] !== undefined) {
         min_round = ladders[chosen_match][0].round_number.length;
-        max_round = ladders[chosen_match][ladders[chosen_match].length-1].round_number.length;
+        max_round = ladders[chosen_match][ladders[chosen_match].length - 1].round_number.length;
     }
 
     let numOfCols = Math.log2(props.calendar_list.places)
 
     return (
         <>
-            {ladders[1][0] !== undefined?
-            <Row>
-                <div>
-                    {props.calendar_list.typeOfLadder === "DRABINKA O MIEJSCA"?
-                        <select value={chosen_match} onChange={(e) => set_chosen_match(e.target.value)}>
-                            <option value="1">Drabinka główna</option>
+            {ladders[1][0] !== undefined ?
+                <Row>
+                    <div>
+                        {props.calendar_list.typeOfLadder === "DRABINKA O MIEJSCA" ?
+                            <select value={chosen_match} onChange={(e) => set_chosen_match(e.target.value)}>
+                                <option value="1">Drabinka główna</option>
                                 <option value="3">3 miejsce</option>
                                 <option value="5">5 miejsce</option>
                                 <option value="7">7 miejsce</option>
-                            {props.calendar_list.places === 16?
-                                <>
-                                    <option value="9">9 miejsce</option>
-                                    <option value="11">11 miejsce</option>
-                                    <option value="13">13 miejsce</option>
-                                    <option value="15">15 miejsce</option>
-                                </>
-                                :null
-                            }
+                                {props.calendar_list.places === 16 ?
+                                    <>
+                                        <option value="9">9 miejsce</option>
+                                        <option value="11">11 miejsce</option>
+                                        <option value="13">13 miejsce</option>
+                                        <option value="15">15 miejsce</option>
+                                    </>
+                                    : null
+                                }
 
-                        </select>
-                        :null
-                    }
-                </div>
+                            </select>
+                            : null
+                        }
+                    </div>
 
 
-            </Row>
-                :null}
+                </Row>
+                : null}
 
             <Row>
                 <SetTournamentRounds
                     {...props}
-                    tournament = {props.calendar_list}
-                    places = {props.calendar_list.places}
-                    pairs_list = {props.pairs_list}
-                    ladders_length = {props.ladders_list.ladders["ALL"].length}
-                    refreshProps = {() => props.refreshProps({...props}, props.id)}
+                    tournament={props.calendar_list}
+                    places={props.calendar_list.places}
+                    pairs_list={props.pairs_list}
+                    ladders_length={props.ladders_list.ladders["ALL"].length}
+                    refreshProps={() => props.refreshProps({...props}, props.id)}
                 />
             </Row>
 
             {props.ladders_list.ladders["ALL"].length !== 0 ?
                 <Row>
                     <DrabinkaRound
-                        net_round = {1}
+                        net_round={1}
                         current_round={min_round}
                         numOfCols={numOfCols}
-                        ladders = {ladders}
-                        chosen_match = {chosen_match}
-                        calendar_list = {props.calendar_list}
-                        user = {props.user}
-                        refreshProps = {() => props.refreshProps({...props}, props.id)}
+                        ladders={ladders}
+                        chosen_match={chosen_match}
+                        calendar_list={props.calendar_list}
+                        user={props.user}
+                        refreshProps={() => props.refreshProps({...props}, props.id)}
                     />
-                    {numOfCols >= 2?
+                    {numOfCols >= 2 ?
                         <DrabinkaRound
-                            net_round = {2}
+                            net_round={2}
                             current_round={min_round + 1}
                             numOfCols={numOfCols}
-                            ladders = {ladders}
-                            chosen_match = {chosen_match}
-                            calendar_list = {props.calendar_list}
-                            user = {props.user}
-                            refreshProps = {() => props.refreshProps({...props}, props.id)}
-                        />:null
+                            ladders={ladders}
+                            chosen_match={chosen_match}
+                            calendar_list={props.calendar_list}
+                            user={props.user}
+                            refreshProps={() => props.refreshProps({...props}, props.id)}
+                        /> : null
                     }
-                    {numOfCols >= 3?
+                    {numOfCols >= 3 ?
                         <DrabinkaRound
-                            net_round = {3}
+                            net_round={3}
                             current_round={min_round + 2}
                             numOfCols={numOfCols}
-                            ladders = {ladders}
-                            chosen_match = {chosen_match}
-                            calendar_list = {props.calendar_list}
-                            user = {props.user}
-                            refreshProps = {() => props.refreshProps({...props}, props.id)}
-                        />:null
+                            ladders={ladders}
+                            chosen_match={chosen_match}
+                            calendar_list={props.calendar_list}
+                            user={props.user}
+                            refreshProps={() => props.refreshProps({...props}, props.id)}
+                        /> : null
                     }
-                    {numOfCols >= 4?
+                    {numOfCols >= 4 ?
                         <DrabinkaRound
-                            net_round = {4}
+                            net_round={4}
                             current_round={min_round + 3}
                             numOfCols={numOfCols}
-                            ladders = {ladders}
-                            chosen_match = {chosen_match}
-                            calendar_list = {props.calendar_list}
-                            user = {props.user}
-                            refreshProps = {() => props.refreshProps({...props}, props.id)}
-                        />:null
+                            ladders={ladders}
+                            chosen_match={chosen_match}
+                            calendar_list={props.calendar_list}
+                            user={props.user}
+                            refreshProps={() => props.refreshProps({...props}, props.id)}
+                        /> : null
                     }
                 </Row> :
                 <Row className="justify-content-md-center">
                     <Col sm={6}>
 
-                        <Row style={{background: "white", marginTop:"10px", borderRadius:"10px", width:"100%"}}>
-                            <my_h4 style={{display:"flex", alignItems:"center", justifyContent:"center", padding:"20px", color: "var(--black)"}}>
+                        <Row style={{background: "white", marginTop: "10px", borderRadius: "10px", width: "100%"}}>
+                            <my_h4 style={{
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "center",
+                                padding: "20px",
+                                color: "var(--black)"
+                            }}>
                                 Brak meczów do wyświetlenia
                             </my_h4>
                         </Row>
@@ -156,13 +164,14 @@ export const Drabinka = (props) => {
             {/*}*/}
 
             {/*{console.log(props.calendar_list.typeOfLadder)}*/}
-            <div style={{marginTop:"20px", marginBottom:"20px", display:"flex", justifyContent:"center"}}>
-                { props.calendar_list.approved === 2 ?
-                (props.calendar_list.typeOfLadder == "DRABINKA O MIEJSCA" ?
-                    <div><End_tournament_places_popup  {...props}/></div> :
-                    <div><EndTournament_popup  {...props}/></div>) :
-                <div><EndUnrankedTournament_popup/></div>
-            }
+            <div style={{marginTop: "20px", marginBottom: "20px", display: "flex", justifyContent: "center"}}>
+                {(props.user.role == props.calendar_list.creator) && (props.calendar_list.approved !== 3) ?
+                    (props.calendar_list.approved === 2 ?
+                        (props.calendar_list.typeOfLadder == "DRABINKA O MIEJSCA" ?
+                            <div><End_tournament_places_popup  {...props}/></div> :
+                            <div><EndTournament_popup  {...props}/></div>) :
+                        <div><EndUnrankedTournament_popup/></div>)
+                :null}
             </div>
 
         </>
