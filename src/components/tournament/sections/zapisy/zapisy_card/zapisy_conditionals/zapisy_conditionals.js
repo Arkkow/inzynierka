fcd0701c38d533251a65354a76301c50b03ad4ed
Lewy <pawlew2@st.amu.edu.pl>
2023-01-18 00:36:46 +0100

    update bugs & features

--- src/components/tournament/sections/zapisy/zapisy_card/zapisy_conditionals/zapisy_conditionals.js
+++ src/components/tournament/sections/zapisy/zapisy_card/zapisy_conditionals/zapisy_conditionals.js
@@ -28,7 +28,7 @@ export const ZapisyConditionals = (props) => {
                                 props.partnerAcceptance === 0 && props.partner !== props.user.id? <paragraph_sb>Czeka na akceptację partnera/rki</paragraph_sb>:
                                     props.partnerAcceptance === 0 && props.partner === props.user.id? null:
                                     // Jeżeli jesteś adminem lub właścicielem turnieju
-                                    (props.user.role === "2" && props.creator === props.user.id) || props.user.role === '3'?
+                                    (props.user.role === "2" && props.tournament.creator === props.user.id) || props.user.role === '3'?
                                         props.paymentstatus === "NOTSTARTED"?null:
                                         props.paymentstatus === "DONE"?
                                             <Form.Check type="switch" defaultChecked="true" disabled={true} style={{
@@ -56,9 +56,10 @@ export const ZapisyConditionals = (props) => {
 
                         }
                         {/** Slider "Zapis 2 opłacony" **/}
+                        {console.log(props)}
                         {props.paymentstatus === "DONE" && props.paymentstatus2 === "DONE"?null:
                             props.partnerAcceptance === 0? null:
-                            (props.user.role === "2" && props.creator === props.user.id) || props.user.role === '3'?
+                            (props.user.role === "2" && props.tournament.creator === props.user.id) || props.user.role === '3'?
                                 props.paymentstatus2 === "NOTSTARTED"?null:
                                 props.paymentstatus2 === "DONE"?
                                     <Form.Check type="switch" defaultChecked="true" disabled={props.paymenttype === "cash"} style={{
@@ -90,8 +91,8 @@ export const ZapisyConditionals = (props) => {
                         {/** Komunikaty dla zawodników **/}
                         {/*Nie może być, bo uruchamia się przed akceptacją zaproszenia*/}
                         {/*|| props.paymentstatus2 === "NOTSTARTED"*/}
-                        {props.userid === props.user.id && (props.paymentstatus2 === "PENDING" )?<paragraph_sb>Oczekuje na płatność partnera/rki</paragraph_sb>:null}
-                        {props.partner === props.user.id && (props.paymentstatus === "PENDING")?<paragraph_sb>Oczekuje na płatność partnera/rki</paragraph_sb>:null}
+                        {props.userid === props.user.id && (props.paymentstatus2 === "PENDING" || props.paymentstatus2 === "NOTSTARTED" )?<paragraph_sb>Oczekuje na płatność partnera/rki</paragraph_sb>:null}
+                        {props.partner === props.user.id && (props.paymentstatus === "PENDING" || props.paymentstatus === "NOTSTARTED")?<paragraph_sb>Oczekuje na płatność partnera/rki</paragraph_sb>:null}
                     </Form>
 
                 </Row>
@@ -101,7 +102,7 @@ export const ZapisyConditionals = (props) => {
                     props.approval==="1" &&
                     (
                         (props.userid === props.user.id && props.paymentstatus !=="DONE" && props.paymentstatus !=="PENDING") ||
-                        (props.partner === props.user.id&& props.paymentstatus2 !=="DONE" && props.paymentstatus2 !=="PENDING")) ?
+                        (props.partner === props.user.id && props.paymentstatus2 !=="DONE" && props.paymentstatus2 !=="PENDING")) ?
                             <Row>
                                 <div style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
                                     <PaymentMethod_popup {...props} rid ={props.id} isOwner={props.userid == props.user.id} />
