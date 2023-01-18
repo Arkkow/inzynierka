9398b591baa444764275b496fe58d4db1034f4ee
Lewy <pawlew2@st.amu.edu.pl>
2023-01-18 02:09:05 +0100

    update conditionals oczekuje na płatność

--- src/components/tournament/sections/zapisy/zapisy_card/zapisy_conditionals/zapisy_conditionals.js
+++ src/components/tournament/sections/zapisy/zapisy_card/zapisy_conditionals/zapisy_conditionals.js
@@ -94,8 +94,8 @@ export const ZapisyConditionals = (props) => {
                         {/** Komunikaty dla zawodników **/}
                         {/*Nie może być, bo uruchamia się przed akceptacją zaproszenia*/}
                         {/*|| props.paymentstatus2 === "NOTSTARTED"*/}
-                        {props.userid === props.user.id && (props.paymentstatus2 === "PENDING" )?<paragraph_sb>Oczekuje na płatność partnera/rki</paragraph_sb>:null}
-                        {props.partner === props.user.id && (props.paymentstatus === "PENDING" )?<paragraph_sb>Oczekuje na płatność partnera/rki</paragraph_sb>:null}
+                        {props.userid === props.user.id && (props.paymentstatus2 === "PENDING" || (props.paymentstatus === "NOTSTARTED" && props.partnerAcceptance === 1))?<paragraph_sb>Oczekuje na płatność partnera/rki</paragraph_sb>:null}
+                        {props.partner === props.user.id && (props.paymentstatus === "PENDING" || props.paymentstatus === "NOTSTARTED")?<paragraph_sb>Oczekuje na płatność partnera/rki</paragraph_sb>:null}
                     </Form>
 
                 </Row>
